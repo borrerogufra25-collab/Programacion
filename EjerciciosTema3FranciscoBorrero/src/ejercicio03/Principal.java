45a010b1ed78291a0876c705bcc9c8b50933f5f8
Francisco Borrero <borrero.gufra25@triana.salesianos.edu>
2025-11-18 09:53:20 +0100

    a

---
+++ EjerciciosTema3FranciscoBorrero/src/ejercicio03/Principal.java
@@ -0,0 +1,15 @@
+package ejercicio03;
+
+/*
+ * Implementar un programa que contenga dos métodos en una clase “operaciones”: un método 
+ * al que se le pase un número entero y diga si este es positivo o negativo y otro que diga 
+ * si un número es par o impar. La clase no debe tener ningún atributo. Se deben probar los métodos en el main.
+ */
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+	}
+
+}
